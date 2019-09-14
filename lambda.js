b7d1186d04e81bcd2964e310e012efb06fbba428
Qinzhi Wang <qinzhi.wang@siemens.com>
2019-09-15 01:25:28 +0800

    Added pg implementation

--- lambda.js
+++ lambda.js
@@ -1,16 +1,36 @@
 'use strict';
+const { Client } = require('pg');
 
-module.exports.handler = function(event, context, callback) {
+module.exports.handler_meters = function(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
   console.log(event); // Contains incoming request data (e.g., query params, headers and more)
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'x-custom-header': 'My Header Value',
-    },
-    body: JSON.stringify({ message: 'Hello World!' }),
-  };
-
-  callback(null, response);
+  const client = new Client();
+  client.connect().then(() => {
+    return client.query('SELECT * FROM park_meter WHERE timestamp >= $1 AND timestamp <= $2', 
+                [event.queryStringParameters.from, event.queryStringParameters.to]).then(res => {
+                  console.log(`result: ${JSON.stringify(res.rows)}`);
+                  callback(null, {statusCode: 200, body: JSON.stringify(res.rows)});
+                });
+  }).catch(err => {
+    console.log(`error: ${err.message}`);
+    callback(err);
+  });
 };
-``
+
+module.exports.handler_meterItem = function(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+  const client = new Client();
+  client.connect().then(() => {
+    return client.query('SELECT * FROM park_meter WHERE id = $1', 
+                [event.pathParameters.id]).then(res => {
+                  console.log(`result: ${JSON.stringify(res.rows[0])}`);
+                  callback(null, {statusCode: 200, body: JSON.stringify(res.rows[0])});
+                });
+  }).catch(err => {
+    console.log(`error: ${err.message}`);
+    callback(err);
+  });
+};
